880c55791abf5ae75e69af5fe70325c6e9ed639b
teradonburi <daikiterai@gmail.com>
2019-06-16 01:02:57 +0900

    認証サンプル

---
+++ index.js
@@ -0,0 +1,154 @@
+// parcelが色々解釈してくれて、importとかreactが使えるようになるよ
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+// ブラウザのlocalStorage領域からtoken情報を取得するよ
+function getUser() {
+  return localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : {}
+}
+
+class TopPage extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {loading: true, user: null}
+    // logoutメソッドの中でthisを使えるようにするよ
+    this.logout = this.logout.bind(this)
+  }
+
+  componentDidMount() {
+    const user = getUser()
+    if (user.id && user.token) {
+      fetch(`/api/users/${user.id}`, {
+        method: 'GET',
+        headers: {'Authorization': `Bearer ${user.token}`},
+      })
+      .then((response) => {
+        if (!response.ok) throw response
+        return response.json()
+      })
+      .then(user => this.setState({user}))
+      .finally(() => this.setState({loading: false}))
+    } else {
+      this.setState({loading: false})
+    }
+  }
+
+  logout() {
+    delete localStorage.user
+    this.setState({user: null})
+  }
+
+  render() {
+    const { loading, user } = this.state
+
+    if (loading) return <div>ローディング中・・・</div>
+
+    return (
+      <div>
+        {!user ? 
+          <Link to='/signup'>ユーザ登録</Link>
+          : 
+          <div>
+            <div>ようこそ{user.name}さん</div>
+            <button onClick={this.logout}>ログアウト</button>
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+class SignupPage extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { error : null }
+    // createUserメソッドの中でthisを使えるようにするよ
+    this.createUser = this.createUser.bind(this)
+  }
+
+  createUser(e) {
+    e.preventDefault() // formのsubmit処理そのものを無効化するよ（ブラウザがリロードされてしまうので）
+    const data = new FormData(e.target)
+    // name属性が指定されたinputタグから値を取得するよ
+    const name = data.get('name')
+    const email = data.get('email')
+    const password = data.get('password')
+    // user作成のAPIを呼ぶよ
+    fetch('/api/users', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({name, email, password}),
+    })
+    .then((response) => {
+      if (!response.ok) throw response
+      return response.json()
+    })
+    .then(user => {
+      // ユーザのidとトークンをブラウザに保存するよ
+      localStorage.setItem('user', JSON.stringify(user))
+      // トップページに遷移するよ
+      this.props.history.push('/')
+    })
+    .catch((e) => {
+      e.json().then((e) => {
+        this.setState({error: e.message})
+      })
+    })
+  }
+
+  render() {
+    const { error } = this.state
+
+    const styles = {
+      input: {
+        display: 'block',
+      },
+      error: {
+        color: 'red',
+      }
+    }
+
+    // ブラウザのlocalStorage領域からtoken情報を取得するよ
+    const user = getUser()
+    if (Object.keys(user).length > 0) {
+      // もうログイン済みなのでトップページに遷移するよ
+      return this.props.history.push('/')
+    }
+
+    return (
+      <form action='url' onSubmit={this.createUser} acceptCharset='UTF-8' >
+        <input name='name' required style={styles.input} placeholder='名前' type='text' />
+        <input name='email' required style={styles.input} placeholder='メールアドレス' type='email' />
+        <input name='password' required minLength='8' style={styles.input} placeholder='パスワード' type='password' autoComplete='suggest' />
+        <input type='submit' value='送信' />
+        <div style={styles.error}>{error}</div>
+      </form>
+    )
+  }
+}
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <BrowserRouter>
+        <Route exact path='/' component={TopPage} />
+        <Route exact path='/signup' component={SignupPage} />
+      </BrowserRouter>
+    )
+  }
+}
+
+// index.htmlのid=rootのdivタグ以下にAppコンポーネント描画を描画するよ
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ server.js
@@ -0,0 +1,128 @@
+// APIのルーティングとかミドルウェアの組み込みが楽だからexpressフレームワークを使うよ
+const express = require('express')
+const app = express()
+
+// ユーザのユニークID生成用
+const uuidv4 = require('uuid/v4')
+// ユーザの情報を保存するデータベース
+// 今回は簡易のためjsonファイルに保存するよ
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json') // 保存するファイル
+const db = low(adapter)
+
+// データベースに入れる初期データ
+db.defaults({users: []})
+  .write()
+
+// サーバーでエラーが起きたらここに到達してエラーログが出ます
+process.on('uncaughtException', (err) => console.error(err))
+process.on('unhandledRejection', (err) => console.error(err))
+
+// POSTメソッドのbodyを受け取れるようにするための前準備の処理
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+// サーバがホスティングするフォルダ
+// サーバ上でindex.htmlが見れるようにするよ
+app.use(express.static('dist'))
+
+// Json WebToken(JWT)生成(改ざん不能なトークン)
+// https://qiita.com/kaiinui/items/21ec7cc8a1130a1a103a
+const jwt = require('jsonwebtoken')
+const secretKey='secret' // 暗号化用キー（バレちゃいけないやつ）
+
+// ハッシュ生成用
+const crypto = require("crypto")
+const sha512 = crypto.createHash('sha512')
+
+// 認証処理の前処理だよ
+const passport = require('passport')
+const BearerStrategy = require('passport-http-bearer')
+const authenticate = passport.authenticate('bearer', {session: false})
+passport.use(new BearerStrategy(function(token, done) {
+  // 認証APIの場合はここを通るよ
+  jwt.verify(token, secretKey, function(err, decoded) {
+    if (err) return done(null, false) // 不正なトークン
+
+    const hash = sha512.update(decoded)
+
+    const user = db.get('users')
+      .find({ password: hash })
+      .value()
+  
+    if (!user) return done(null, false) // パスワードがユーザのものと一致しない
+    return done(null, user) // 認証OK
+  })
+}))
+
+// ユーザ作成
+async function create(req, res) {
+  // POSTメソッドのパラメータはbodyに渡ってきます。
+  const name = req.body.name
+  const email = req.body.email
+  const password = req.body.password
+
+  const user = db.get('users')
+    .find({ email })
+    .value()
+
+  if (user) {
+    return res.status(400).json({message: 'すでにそのメールアドレスは使われています'})
+  }
+
+  // passwordはデータベースに直接保存するのでなくハッシュ化する
+  // 生のパスワードを保存しておくと、データベースをまるごと盗まれたらやばい
+  const hash = sha512.update(password)
+
+  // user作成
+  const id = uuidv4()
+  db.get('users')
+    .push({id, name, email, password: hash}) 
+    .write()
+  
+  // JWT作成（認証用）
+  const token = jwt.sign(password, 'secret')
+  res.json({id, token})
+}
+
+// ユーザの取得
+async function show(req, res) {
+  // :idの部分にユーザのidが渡ってきます。(req.params.idで参照)
+  const user = db.get('users')
+    .find({ id: req.params.id })
+    .value()
+  if (user === null) return res.status(404).json({message: 'not found'})
+  res.json(user)
+}
+
+// 認証なしのAPI
+app.use(
+  '/api',
+  express.Router()
+    // ユーザの作成
+    .post('/users', create) // POSTメソッド
+)
+
+// 認証ありのAPI
+app.use(
+  '/api',
+  authenticate, // 認証処理
+  express.Router()
+    // ログイン済みユーザの取得
+    .get('/users/:id', show)
+)
+
+// 開発用の設定だよ
+if (process.env.NODE_ENV === 'dev') {
+  const Bundler = require('parcel-bundler')
+  const bundler = new Bundler('index.html')
+  // index.htmlが使っているモジュールが変更されたらクライアント側を再ビルドするよ
+  app.use(bundler.middleware())  
+}
+
+// サーバを5000番ポートで起動するよ
+app.listen(5000, () => {
+  console.log('ブラウザでhttp://localhost:5000にアクセスしてください')
+})
